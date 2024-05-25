eb6d9712d562c5bb459e783133fb2ef50ecfefad
Jorge-Martinez82 <118907308+Jorge-Martinez82@users.noreply.github.com>
2024-05-25 19:58:16 +0200

    Update config.php

--- src/config.php
+++ src/config.php
@@ -1,9 +1,9 @@
 <?php
 
 // defino la constante para la URL base de Firestore
-define('FIRESTORE_URL', 'https://firestore.googleapis.com/v1/projects/proyectoreflejos-314e8/databases/(default)/documents/');
+define('FIRESTORE_URL', 'yourDBURL');
 // defino la constante para la clave de la API de Firestore
-define('FIRESTORE_API_KEY', 'AIzaSyDbSuePnzrESx7GIoFiL9niGF_N9NVJahs');
+define('FIRESTORE_API_KEY', 'yourAPIKEY');
 
 ?>
 
